4e96196dde4b0a0da14077f3be4042b0757a7bb3
MADHUSREE BERA <73298219+MadhusreeBera@users.noreply.github.com>
2022-09-15 17:22:40 +0530

    lab 6

---
+++ ssd_lab_activity_6/README.txt
@@ -0,0 +1,4 @@
+I have used onkeyup event handler to detect the input field values
+I have used REGEX to validate server username
+For saving the elements of drag and drop,
+I have used childrenNodes properties of the div2 element and stored all the innerText to store the names
---
+++ ssd_lab_activity_6/myscripts.js
@@ -0,0 +1,82 @@
+var flag = true;
+function handleSubmit() {
+  console.log("clicked");
+  const name = document.getElementById("nameid").value;
+  const email = document.getElementById("emailid").value;
+
+  const username = document.getElementById("sname").value;
+
+  const lead = document.getElementById("lead").value;
+
+  var members = "";
+
+  const children = document.getElementById("div2").childNodes;
+  console.log(children);
+  children.forEach((ele) => {
+    members += "\n" + ele.innerText;
+    //   console.log(ele.id);
+    console.log(ele.innerText);
+  });
+  if (name != "" && email != "" && username != "" && lead != "" && flag) {
+    alert(
+      "Name: " +
+        name +
+        "\nEmail: " +
+        email +
+        "\nUsername: " +
+        username +
+        "\nTeam Lead: " +
+        lead +
+        "\n\nTeam Members:\n" +
+        members
+    );
+  } else {
+    alert("Fill properly");
+  }
+}
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+function f(event) {
+  const s = document.querySelector("#sname").value;
+  const err = document.getElementById("error_msg");
+  isValid = false;
+  if (s != "") {
+    if (hasUpperCase(s) && hasDigit(s)) {
+      // console.log("valid");
+      err.innerHTML = "";
+      flag = true;
+    } else {
+      // console.log("invalid");
+      err.innerHTML = "Invalid Username";
+      flag = false;
+    }
+  }
+}
+
+function hasUpperCase(str) {
+  return /[A-Z]/.test(str);
+}
+function hasDigit(str) {
+  return /[0-9]/.test(str);
+}
+const checkPass = () => {
+  const oldp = document.getElementById("pass");
+  const conp = document.getElementById("conp");
+  if (oldp.value !== conp.value) {
+    alert("Re-enter password correctly!");
+    flag = false;
+  } else {
+    flag = true;
+  }
+};
